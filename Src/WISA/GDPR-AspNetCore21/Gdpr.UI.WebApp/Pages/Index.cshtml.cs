e1f81079cdb0047bee0e166ec7354477a13b8a56
Will Stott <will.stott@hotmail.com>
2018-11-07 14:17:57 +0530

    v 1.1.30.0 - Added new WebApp ASP.Net Core 2.1 Razor Pages with support for MxReturnCode

---
+++ Src/WISA/GDPR-AspNetCore21/Gdpr.UI.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+using Gdpr.Domain;
+
+namespace Gdpr.UI.WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration _config;
+        public string Status { get; set; }
+        public string URDCount { get; set; }
+
+        public IndexModel(IConfiguration config)
+        {
+           _config = config;
+        }
+
+        public async Task OnGet()
+        {
+            URDCount = "Failed";
+            var conn = _config.GetConnectionString("DefaultConnection");
+            using (IAdminRepository repository = new AdminRepository(conn))
+            {
+                var resCnt = await repository.GetRoleCountAsync();
+                if (resCnt.IsError())
+                    Status = resCnt.GetErrorUserMsg();
+                else
+                {
+                    Status = "Success";
+                    URDCount = String.Format("URD Count = {0}", resCnt.GetResult());
+                }
+            }
+        }
+    }
+}
---
+++ Src/WISA/GDPR-AspNetCore21/Gdpr.UI.WebApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+using MxReturnCode;
+
+namespace Gdpr.UI.WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MxUserMsg.Init(Assembly.GetExecutingAssembly(), MxMsgs.SupportedCultures);
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
